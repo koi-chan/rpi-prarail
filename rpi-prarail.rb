2b81cc6ce7bf46429d83f81f83a4a69e1a531ef7
koi-chan <koi-chan@mail.kazagakure.net>
2019-07-21 13:23:58 +0900

    バグ修正

--- rpi-prarail.rb
+++ rpi-prarail.rb
@@ -11,10 +11,10 @@ SLEEP = 0.01
 =begin
 MODE = 0 (IN/IN)
 IN1 IN2 OUT1 OUT2
-  0   0    -    -  空転
+  0   0    -    -  空転 (coast)
   0   1    L    H  逆進
   1   0    H    L  前進
-  1   1    L    L  回生ブレーキ
+  1   1    L    L  回生ブレーキ (break)
 
 MODE = 1 (PHASE/ENABLE)
 IN1 IN2 OUT1 OUT2
@@ -39,19 +39,19 @@ class RpiPrarail
   end
 
   def pwm_setting(freq, duty)
-    @pwm = PWM.new(freq: freq, duty: duty)
+    @pwm = PWM.new(new_freq: freq, new_duty: duty)
   end
 
   def start
     if(@pwm)
       loop do
-        _start
-        sleep(@pwm.pulse_on - SLEEP)
+        _forward
+        sleep(@pwm.pulse_on)
         stop
-        sleep(@pwm.pulse_off - SLEEP)
+        sleep(@pwm.pulse_off)
       end
     else
-      _start
+      _forward
     end
   end
 
@@ -62,25 +62,25 @@ class RpiPrarail
   def _coast
     @pin_in1.off
     @pin_in2.off
-    _sleep
+    #_sleep
   end
 
   def _forward
     @pin_in1.on
     @pin_in2.off
-    _sleep
+    #_sleep
   end
 
   def _reverse
     @pin_in1.off
     @pin_in2.on
-    _sleep
+    #_sleep
   end
 
   def _brake
     @pin_in1.on
     @pin_in2.on
-    _sleep
+    #_sleep
   end
 
   private
@@ -113,13 +113,8 @@ class RpiPrarail::PWM
   attr_reader :pulse_off
 
   def initialize(new_freq: 50.0, new_duty: 0.0)
-    freq = new_freq
-    duty = new_duty
-  end
-
-  def duty=(new_duty)
-    @duty = new_duty if(new_duty >= 0.0 && new_duty <= 100.0)
-    get_pulse
+    self.freq = new_freq
+    self.duty = new_duty
   end
 
   private
@@ -128,16 +123,22 @@ class RpiPrarail::PWM
     @freq = new_freq if(new_freq > 0.0)
   end
 
+  def duty=(new_duty)
+    @duty = new_duty if(new_duty >= 0.0 && new_duty <= 100.0)
+    get_pulse
+  end
+
   def get_pulse
-    @pulse_on = 1 / freq * duty / 100
-    @pulse_off = 1 - @pulse_on
+    @pulse_on = 1.0 / freq * duty / 100
+    @pulse_off = 1.0 / freq - @pulse_on
   end
 end
 
 puts('program started.')
-prarail = RpiPrarail.new
+@prarail = RpiPrarail.new
+@prarail.pwm_setting(50, 16)
 
 puts('testing...')
-prarail.start
-sleep(1)
-prarail.stop
+@prarail.start
+sleep(10)
+@prarail.stop
